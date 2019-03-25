709defe6879f1d98b1b350bc4ad1a86eee07ba9d
John <japarker@uchicago.edu>
2019-03-24 23:30:51 -0500

    sphere-plane collisions

--- pedesis/collisions.py
+++ pedesis/collisions.py
@@ -2,7 +2,15 @@ import numpy as np
 from pedesis import interactions
 
 class hard_sphere_collisions(interactions):
+    """
+    Collision between spheres using the force model F = kn*overlap^1.5
+    """
     def __init__(self, radii, kn):
+        """
+        Arguments:
+            radii   particle radii
+            kn      force constant
+        """
         # self.radii = pedesis.array(radii)
         self.radii = radii
         self.kn = kn
@@ -33,7 +41,36 @@ class hard_sphere_collisions(interactions):
     def torque(self):
         return np.zeros_like(self.position)
 
+
 class hard_sphere_plane_collision(interactions):
-    def __init__(self, radii, kn):
+    """
+    Collision between spheres and a planar surface using the force model F = kn*overlap^1.5
+    """
+    def __init__(self, radii, kn, zpos=0):
+        """
+        Arguments:
+            radii   particle radii
+            kn      force constant
+            zpos    z-position of the plane (default: 0)
+        """
         self.radii = radii
         self.kn = kn
+        self.zpos = zpos
+
+    def force(self):
+        Nparticles = len(self.position)
+        if np.isscalar(self.radii):
+            rad = np.full(Nparticles, self.radii, dtype=float)
+        else:
+            rad = np.asarray(self.radii, dtype=float)
+
+        F = np.zeros_like(self.position)
+
+        dz = self.position[:,2] - self.zpos
+        idx = np.abs(dz) < rad
+        F[idx,2] = np.sign(dz)*self.kn*dz**1.5
+
+        return F
+
+    def torque(self):
+        return np.zeros_like(self.position)
